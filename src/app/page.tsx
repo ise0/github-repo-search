e67033e356e1da2e9bf5590bd01e79504ede08b1
root <root@LAPTOP-7CK1TGOO>
2024-08-18 23:39:49 +0300

    provide rtk store & display all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
+'use client'
+import { Header } from "@/components/header";
+import { Box, Typography } from "@mui/material";
+import { RepoList } from "@/components/repo-list";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "@/api";
+import { useState } from "react";
+import "./globals.scss"
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+})
+
 export default function Home() {
+  const [search, setSearch] = useState('')
+
   return (
-    <main>
-      hi
-    </main>
+    <Provider store={store}>
+      <Box height='100vh' display="flex" flexDirection="column" >
+        <Box minHeight={'76px'} />
+        <Header value={search} onChange={(v) => setSearch(v)} />
+        {search == '' ?
+          <Typography m='auto' variant="h4" component="h2">Добро пожаловать</Typography> :
+          <RepoList search={search} />
+        }
+        <Box bgcolor='#4F4F4F' minHeight='32px' width='100%' alignSelf='flex-end' />
+      </Box>
+    </Provider>
   );
 }
